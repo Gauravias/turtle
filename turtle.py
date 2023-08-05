f692ac2044123e53b058a10b1fb323e8aaea79a6
Gaurav Tripathi <>
2023-08-05 14:02:55 +0530

    k

--- turtle.py
+++ turtle.py
@@ -1,2 +1,12 @@
-@
-@
+import turtle
+
+# Create a turtle object
+my_turtle = turtle.Turtle()
+
+# Draw a square
+for _ in range(4):
+    my_turtle.forward(100)  # Move the turtle forward by 100 units
+    my_turtle.left(90)     # Turn the turtle 90 degrees to the left
+
+# Keep the window open until it is manually closed
+turtle.done()
